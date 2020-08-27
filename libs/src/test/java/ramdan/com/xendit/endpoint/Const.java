11346a88d0b33258b5f0c4a532944da68a3e3f14
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2020-08-27 23:35:55 +0700

    Add commit Const

---
+++ libs/src/test/java/ramdan/com/xendit/endpoint/Const.java
@@ -0,0 +1,10 @@
+package ramdan.com.xendit.endpoint;
+
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+
+public class Const {
+    
+    public static BasicAuthenticationInterceptor basicAuth =
+            new BasicAuthenticationInterceptor("xnd_public_development_OYqIfOUth+GowsY6LeJOHzLCZtSj84J9kXDn+Rxj/mbf/LCoCQdxgA==", "");
+
+}
